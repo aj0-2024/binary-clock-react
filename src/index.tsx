f57c671035e7af0f4b6bd6732ddc4cd750f87f87
Anirudh Janga <j.anirudh@icloud.com>
2020-10-11 22:14:49 -0500

    Adding empty binary clock component

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
-import React, { FC, HTMLAttributes, ReactChild } from 'react';
+import React, { FC, HTMLAttributes, ReactChild } from "react";
+
+interface BulbColor {
+    offColor: string;
+    onColor: string;
+}
+
+interface Skin {
+    Default;
+}
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
-  /** custom content, defaults to 'the snozzberries taste like snozzberries' */
-  children?: ReactChild;
+    theme?: BulbColor;
+    skin?: Skin;
+    children?: ReactChild;
 }
 
 // Please do not use types off of a default export module or else Storybook Docs will suffer.
 // see: https://github.com/storybookjs/storybook/issues/9556
+
 /**
- * A custom Thing component. Neat!
+ * A binary clock component
  */
-export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
+export const BinaryClock: FC<Props> = () => {
+    return <div />;
 };
