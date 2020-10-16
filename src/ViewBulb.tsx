c5e62156cf2dd0a914758d7b83ff0ab84dea7cf1
Anirudh Janga <j.anirudh@icloud.com>
2020-10-15 22:22:12 -0500

    Keeping it minimal

--- src/ViewBulb.tsx
+++ src/ViewBulb.tsx
@@ -12,7 +12,6 @@ const useStyles = createUseStyles({
         borderRadius: 4,
         transition: "all 0.3s",
         transitionTimingFunction: "cubic-bezier(.25,.8,.25,1)",
-        boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
     },
     on: {
         background: (theme: Theme) => theme.primaryColor,
