d664d5ceefacb40ca24107062f69cb619a45dc79
Anirudh Janga <j.anirudh@icloud.com>
2020-10-24 22:12:27 -0500

    Implemented a custom hook for getting currDateTime updated every second

--- src/BinaryClock.tsx
+++ src/BinaryClock.tsx
@@ -1,6 +1,5 @@
-import React, { FC, HTMLAttributes, ReactChild, useState } from "react";
+import React, { FC, HTMLAttributes, ReactChild, useState, useEffect } from "react";
 import ViewTimeUnit from "./ViewTimeUnit";
-import useInterval from "@use-it/interval";
 import { getTime } from "binary-clock-core";
 import { Theme, ThemeContext } from "./ThemeContext";
 import "./BinaryClock.css";
@@ -11,22 +10,34 @@ export interface BinaryClockProps extends HTMLAttributes<HTMLDivElement> {
     children?: ReactChild;
 }
 
+const useCurrDateTime = () => {
+    const [ currDateTime, setCurrDateTime ] = useState<Date>(new Date());
+
+    // updated the currDateTime value every second
+    const updateInterval = 1000;
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrDateTime(new Date());
+        }, updateInterval);
+
+        return () => clearInterval(intervalId);
+    });
+
+    return currDateTime;
+}
+
 /**
  * A binary clock component
  */
 export const BinaryClock: FC<BinaryClockProps> = (props) => {
-    const [currDate, setDate] = useState<Date>(new Date());
-
-    useInterval(() => {
-        setDate(new Date());
-    }, 1000);
+    const currDateTime = useCurrDateTime();
 
     const theme: Theme = {
         bulbOnClassName: props.bulbOnClassName,
         bulbOffClassName: props.bulbOffClassName,
     };
 
-    const currTime = getTime(currDate);
+    const currTime = getTime(currDateTime);
     return (
         <ThemeContext.Provider value={theme}>
             <div className="bin-clock-container">
